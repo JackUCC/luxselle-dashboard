6937a6f7de770a2ae16a25491d588157924abf91
JackUCC <122489574@umail.ucc.ie>
2026-01-26 13:18:30 +0000

    Initial commit - Luxselle Dashboard

---
+++ src/LuxselleApp.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LuxselleApp: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <h1 className="text-3xl font-bold text-center py-8">Luxselle Dashboard</h1>
+    </div>
+  )
+}
+
+export default LuxselleApp
